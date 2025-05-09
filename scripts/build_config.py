424ce4cf2b37676e5694bd99d07904cd2053c2ec
lanseria <zhangchao564265135@hotmail.com>
2025-05-09 17:08:11 +0800

    refactor(build): 移除不再使用的构建脚本和配置文件

--- scripts/build_config.py
+++
@@ -1,5 +0,0 @@
-"""Configuration for building executable."""
-from pathlib import Path
-from PyInstaller.utils.hooks import collect_data_files
-# Build Paths
-ROOT_DIR = Path(__file__).parent.parent
